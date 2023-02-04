26a87ee44994d7567805020add24824d14a08dee
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2023-02-04 09:43:58 +0530

    Added Landing Page✅

---
+++ src/components/Main2.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import styles from "../style";
+
+const Main2 = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+  >
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Finding events in your locality has never been easier. Try our service.
+      </p>
+    </div>
+
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <div>
+        <Link
+          to={"/events"}
+          className="inline-flex items-center w-full px-5 py-3 mb-3 mr-1 text-base font-semibold text-white no-underline align-middle bg-blue-600 border border-transparent border-solid rounded-md cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-blue-700 hover:border-blue-700 hover:text-white focus-within:bg-blue-700 focus-within:border-blue-700"
+        >
+          Find Events
+          <svg
+            class="w-4 h-4 ml-2"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M14 5l7 7m0 0l-7 7m7-7H3"
+            ></path>
+          </svg>
+        </Link>
+      </div>
+    </div>
+  </section>
+);
+
+export default Main2;
